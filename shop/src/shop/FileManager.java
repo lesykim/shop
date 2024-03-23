732432177f0a1911d16f0c08af54296d0e2136ac
lesykim <trchol01@naver.com>
2024-03-23 18:31:58 +0900

    Add FileManager()

--- shop/src/shop/FileManager.java
+++ shop/src/shop/FileManager.java
@@ -1,5 +1,20 @@
 package shop;
 
-public class FileManager {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
+public class FileManager {
+	private FileWriter fileWriter;
+	private FileReader fileReader;
+	private BufferedReader bufferedReader;
+	
+	private File file;
+	private String fileName;
+	
+	public FileManager() {
+		fileName = "shop.txt";
+		file = new File(fileName);
+	}
 }
